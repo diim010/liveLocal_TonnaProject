c100efb3746e8528a9576baa2d65e02db39b0481
diim <dmitry.besmertny@gmail.com>
2025-04-10 00:53:18 +0300

    slider block styles

--- template-parts/blocks/block-slider.php
+++ template-parts/blocks/block-slider.php
@@ -7,27 +7,33 @@
 
 
   ?>
-<div>
 
-        <div class="uk-h3">Scale</div>
+
+<section class="block-slider">
+
+<?php if ( have_rows('slider') ) : ?>
 
         <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="animation: scale">
 
-            <div class="uk-slideshow-items">
-                <div>
-                    <img src="images/photo.jpg" alt="" uk-cover>
+            <div class="block-slider__wrap uk-slideshow-items">
+            <?php while( have_rows('slider') ) : the_row(); ?>
+
+                <div class="block-slider__item uk-height-large uk-background-cover uk-light" style="background-image: url(<?php echo esc_url(get_sub_field('slide_img')['url']); ?>);">
+                    <img src="<?php echo esc_url(get_sub_field('slide_img')['url']); ?>" alt="<?php echo esc_attr(get_sub_field('slide_img')['alt']); ?>" uk-cover>
+                    <div class="block-slider__item-content uk-position-center uk-position-small uk-text-center">
+                      <h1 class="uk-margin-remove"><?php the_sub_field('slide_title'); ?></h1>
+                      <p><?php the_sub_field('slide_text'); ?></p>
+                      <a href="<?php the_sub_field('slide_button_link'); ?>" class="uk-button uk-button-default"><?php the_sub_field('slide_button_name'); ?></a>
+                    </div>
                 </div>
-                <div>
-                    <img src="images/dark.jpg" alt="" uk-cover>
-                </div>
-                <div>
-                    <img src="images/light.jpg" alt="" uk-cover>
-                </div>
+
+            <?php endwhile; ?>
+
             </div>
 
             <a class="uk-position-center-left uk-position-small uk-hidden-hover" href uk-slidenav-previous uk-slideshow-item="previous"></a>
             <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next uk-slideshow-item="next"></a>
 
         </div>
-
-    </div>
+    <?php endif; ?>
+</section>
